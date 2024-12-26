3305793a847b0d44fb4a65d397f16fedb3531172
YJ-ArchLinux-Master <yj1516268@outlook.com>
2024-12-26 15:55:19 +0800

    新增 infomsg

---
+++ infomsg.go
@@ -0,0 +1,35 @@
+/*
+File: infomsg.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2024-11-03 12:50:35
+
+Description: 输出格式化后的常规信息
+*/
+
+package msgcenter
+
+import (
+	"github.com/gookit/color"
+	"github.com/yhyj/msgcenter/general"
+)
+
+// PrintInfoMsg 格式化原始常规信息后输出
+//
+//   - 输出格式为：常规信息
+//
+// 参数：
+//   - info: 原始常规信息
+func PrintInfoMsg(info string) {
+	color.Printf("%s\n", general.LightText(info))
+}
+
+// PrintSuccessMsg 格式化原始成功信息后输出
+//
+//   - 输出格式为：成功信息
+//
+// 参数：
+//   - info: 原始成功信息
+func PrintSuccessMsg(success string) {
+	color.Printf("%s %s\n", general.SuccessFlag, general.SuccessText(success))
+}
